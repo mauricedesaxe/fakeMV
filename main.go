05ee932c862bcdf0adc8b5c8cffeb1de27fb4b10
lazaralex98 <alex2012ro@gmail.com>
2024-07-27 19:22:21 +0300

    test in tests

--- main.go
+++ main.go
@@ -4,80 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"os"
-	"time"
-
-	"math/rand"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	// open the database
-	db, err := sql.Open("sqlite3", "./test.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// apply pragmas to make the database faster
-	db.Exec("PRAGMA journal_mode = WAL;")
-	db.Exec("PRAGMA synchronous = NORMAL;")
-	db.Exec("PRAGMA cache_size = -64000;")
-	db.Exec("PRAGMA temp_store = MEMORY;")
-
-	// create the table
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS cash_flow_events (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		amount INTEGER NOT NULL, 
-		date TIMESTAMP NOT NULL, 
-		category TEXT NOT NULL,  -- income / expense
-		necessity TEXT NOT NULL, -- need / want
-		description TEXT, 		 -- optional
-		user_id INTEGER NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-		deleted_at TIMESTAMP
-	)`)
-	if err != nil {
-		log.Fatalf("Error creating cash_flow_events table: %v", err)
-	}
-
-	// get events count
-	var count int
-	db.QueryRow("SELECT COUNT(*) FROM cash_flow_events").Scan(&count)
-	if count < 1000 {
-		// seed with fake events up to 1000
-		for i := 0; i < 1000-count; i++ {
-			db.Exec(`
-			INSERT INTO cash_flow_events (amount, date, category, necessity, description, user_id) 
-			VALUES (?, ?, ?, ?, ?, ?)`, rand.Intn(10000), time.Now().AddDate(0, 0, rand.Intn(30)), "income", "need", "description", rand.Intn(100))
-		}
-	}
-
-	// log new events count
-	db.QueryRow("SELECT COUNT(*) FROM cash_flow_events").Scan(&count)
-	log.Printf("Events count: %d", count)
-
-	// get a sample of 5 events
-	rows, err := db.Query(`SELECT id, amount, category, user_id FROM cash_flow_events LIMIT 5`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	logTable(rows)
-
-	prepareMVCentralStore(db)
-	createMV(db, `SELECT id, amount, category, user_id FROM cash_flow_events LIMIT 5`, "cash_flow_events_mv")
-
-	// remove the last 5 events
-	db.Exec(`DELETE FROM cash_flow_events WHERE id IN (SELECT id FROM cash_flow_events ORDER BY id DESC LIMIT 5)`)
-
-	// refresh the material view
-	refreshMV(db, "cash_flow_events_mv")
-
-	// delete db file
-	os.Remove("./test.db")
+	log.Println("hello")
 }
 
 // creates a table where we store material views => query connections
@@ -112,7 +45,7 @@ func createMV(db *sql.DB, query string, mvName string) error {
 	}
 
 	// Create table schema
-	createTableSQL := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (", mvName)
+	createTableSQL := fmt.Sprintf("CREATE TABLE IF NOT EXISTS [%s] (", mvName)
 	for i, ct := range columnTypes {
 		if i > 0 {
 			createTableSQL += ", "
@@ -120,7 +53,6 @@ func createMV(db *sql.DB, query string, mvName string) error {
 		createTableSQL += fmt.Sprintf("%s %s", ct.Name(), sqliteType(ct.DatabaseTypeName()))
 	}
 	createTableSQL += ")"
-	log.Println("creation query:", createTableSQL)
 
 	// Create the table
 	_, err = db.Exec(createTableSQL)
@@ -142,13 +74,13 @@ func createMV(db *sql.DB, query string, mvName string) error {
 	}
 
 	// Drop data of table
-	_, err = tx.Exec(fmt.Sprintf("DELETE FROM %s", mvName))
+	_, err = tx.Exec(fmt.Sprintf("DELETE FROM [%s]", mvName))
 	if err != nil {
 		return fmt.Errorf("error deleting data: %w", err)
 	}
 
 	// Insert data into the new table
-	insertSQL := fmt.Sprintf("INSERT INTO %s SELECT * FROM (%s)", mvName, query)
+	insertSQL := fmt.Sprintf("INSERT INTO [%s] SELECT * FROM (%s)", mvName, query)
 	_, err = tx.Exec(insertSQL)
 	if err != nil {
 		return fmt.Errorf("error inserting data: %w", err)
@@ -158,16 +90,6 @@ func createMV(db *sql.DB, query string, mvName string) error {
 	if err = tx.Commit(); err != nil {
 		return fmt.Errorf("error committing transaction: %w", err)
 	}
-
-	log.Println("insertion query:", insertSQL)
-
-	// read the data from the new table
-	rows, err = db.Query(fmt.Sprintf("SELECT * FROM %s", mvName))
-	if err != nil {
-		return fmt.Errorf("error reading data: %w", err)
-	}
-	defer rows.Close()
-	logTable(rows)
 
 	return nil
 }
---
+++ main_test.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"database/sql"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+)
+
+var db *sql.DB
+var err error
+
+func TestSetupDb(t *testing.T) {
+	db, err = sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		t.Errorf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	// apply pragmas to make the database faster
+	db.Exec("PRAGMA journal_mode = WAL;")
+	db.Exec("PRAGMA synchronous = NORMAL;")
+	db.Exec("PRAGMA cache_size = -64000;")
+	db.Exec("PRAGMA temp_store = MEMORY;")
+
+	// create the table
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS cash_flow_events (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		amount INTEGER NOT NULL, 
+		date TIMESTAMP NOT NULL, 
+		category TEXT NOT NULL,  -- income / expense
+		necessity TEXT NOT NULL, -- need / want
+		description TEXT, 		 -- optional
+		user_id INTEGER NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		deleted_at TIMESTAMP
+	)`)
+	if err != nil {
+		t.Errorf("Error creating cash_flow_events table: %v", err)
+	}
+
+	// get events count
+	var count int
+	db.QueryRow("SELECT COUNT(*) FROM cash_flow_events").Scan(&count)
+	if count < 1000 {
+		// seed with fake events up to 1000
+		for i := 0; i < 1000-count; i++ {
+			db.Exec(`
+			INSERT INTO cash_flow_events (amount, date, category, necessity, description, user_id) 
+			VALUES (?, ?, ?, ?, ?, ?)`, rand.Intn(10000), time.Now().AddDate(0, 0, rand.Intn(30)), "income", "need", "description", rand.Intn(100))
+		}
+	}
+
+	// expect 1000 events
+	db.QueryRow("SELECT COUNT(*) FROM cash_flow_events").Scan(&count)
+	if count != 1000 {
+		t.Errorf("Expected 1000 events, got %d", count)
+	}
+}
+
+func TestCreateMV(t *testing.T) {
+	// open database
+	db, err = sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		t.Errorf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	// get a rawSample of 5 events
+	rawSample, err := db.Query(`SELECT id, amount, category, user_id FROM cash_flow_events LIMIT 5`)
+	if err != nil {
+		t.Errorf("Error getting data from raw table: %v", err)
+	}
+	defer rawSample.Close()
+
+	err = prepareMVCentralStore(db)
+	if err != nil {
+		t.Errorf("Error preparing material view central store: %v", err)
+	}
+	err = createMV(db, `SELECT id, amount, category, user_id FROM cash_flow_events LIMIT 5`, "cash_flow_events_mv")
+	if err != nil {
+		t.Errorf("Error creating material view: %v", err)
+	}
+
+	// get data from the material view
+	mvSample, err := db.Query(`SELECT * FROM cash_flow_events_mv`)
+	if err != nil {
+		t.Errorf("Error getting data from material view: %v", err)
+	}
+	defer mvSample.Close()
+
+	// test if the firstMvSample is the same as the firstRawSample
+	for rawSample.Next() && mvSample.Next() {
+		var id1, id2 int
+		var amount1, amount2 int
+		var category1, category2 string
+		var user_id1, user_id2 int
+		err = rawSample.Scan(&id1, &amount1, &category1, &user_id1)
+		if err != nil {
+			t.Errorf("Error scanning raw sample: %v", err)
+		}
+		err = mvSample.Scan(&id2, &amount2, &category2, &user_id2)
+		if err != nil {
+			t.Errorf("Error scanning MV sample: %v", err)
+		}
+		if id1 != id2 || amount1 != amount2 || category1 != category2 || user_id1 != user_id2 {
+			t.Errorf("Mismatch: Raw(%d, %d, %s, %d) != MV(%d, %d, %s, %d)",
+				id1, amount1, category1, user_id1,
+				id2, amount2, category2, user_id2)
+		}
+	}
+}
+
+func TestRefreshMV(t *testing.T) {
+	// open database
+	db, err = sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		t.Errorf("Error opening database: %v", err)
+	}
+	defer db.Close()
+
+	// remove the last 5 events & refresh the material view
+	_, err = db.Exec(`DELETE FROM cash_flow_events WHERE id IN (SELECT id FROM cash_flow_events ORDER BY id DESC LIMIT 5)`)
+	if err != nil {
+		t.Errorf("Error deleting events: %v", err)
+	}
+	err = refreshMV(db, "cash_flow_events_mv")
+	if err != nil {
+		t.Errorf("Error refreshing material view: %v", err)
+	}
+
+	// get data from raw table and from material view
+	rawSample, err := db.Query(`SELECT id FROM cash_flow_events ORDER BY id DESC LIMIT 5`)
+	if err != nil {
+		t.Errorf("Error getting data from raw table: %v", err)
+	}
+	mvSample, err := db.Query(`SELECT * FROM cash_flow_events_mv`)
+	if err != nil {
+		t.Errorf("Error getting data from material view: %v", err)
+	}
+
+	// test if the secondMvSample is the same as the secondRawSample
+	for rawSample.Next() && mvSample.Next() {
+		var id1, id2 int
+		var amount1, amount2 int
+		var category1, category2 string
+		var user_id1, user_id2 int
+		err = rawSample.Scan(&id1, &amount1, &category1, &user_id1)
+		if err != nil {
+			t.Errorf("Error scanning raw sample: %v", err)
+		}
+		err = mvSample.Scan(&id2, &amount2, &category2, &user_id2)
+		if err != nil {
+			t.Errorf("Error scanning MV sample: %v", err)
+		}
+		if id1 != id2 || amount1 != amount2 || category1 != category2 || user_id1 != user_id2 {
+			t.Errorf("Mismatch: Raw(%d, %d, %s, %d) != MV(%d, %d, %s, %d)",
+				id1, amount1, category1, user_id1,
+				id2, amount2, category2, user_id2)
+		}
+	}
+}
+
+func TestCleanup(t *testing.T) {
+	// delete db file
+	os.Remove("./test.db")
+}
